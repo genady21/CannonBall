5aac01367b080cf822fc08d37a07528a0f62c252
Valery <wazgenby@gmail.com>
2023-12-18 23:53:42 +0100

    AddRestrictions

--- CannonBall/Assets/Scripts/Cannon/RotateBase.cs
+++ CannonBall/Assets/Scripts/Cannon/RotateBase.cs
@@ -20,7 +20,9 @@ namespace Cannon
       }
       public override void Rotate(Vector2 vector)
       {
-          current += vector;
+         // current += vector;
+          current.x = Mathf.Clamp(current.x + vector.x, -55, 55);
+         current.y = Mathf.Clamp(current.y - vector.y, -30, 0);
           ApplyRotation();
       }
 
